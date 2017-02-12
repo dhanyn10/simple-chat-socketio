0508ffda7cf5bb743a67a1557e04fe37f926e9f4
dhanyn10 <dhanyn.dhan@gmail.com>
2017-02-12 11:32:25 +0700

    update, fix problem when running on heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var app     = require('express')();
 var http    = require('http').Server(app);
 var io      = require('socket.io')(http);
-
+var port    = process.env.PORT || 3000;
 var count   = 0;
 
 app.get('/', function(req,res){
@@ -36,6 +36,6 @@ io.on('connection', function(socket){
     });
 });
 
-http.listen(3000, function(){
-    console.log('listening port 3000');
+http.listen(port, function(){
+    console.log('listening port ' + port);
 });
